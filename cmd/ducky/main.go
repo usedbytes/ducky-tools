d150955db6d3ee5bb8ee6609ce1c54c163c89ba3
Brian Starkey <stark3y@gmail.com>
2020-03-22 17:58:26 +0000

    cmd/ducky: Add iap test command
    This attempts connection, data read, and reset into and out of IAP

--- cmd/ducky/main.go
+++ cmd/ducky/main.go
@@ -3,15 +3,18 @@
 package main
 
 import (
-	"io/ioutil"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
+	"github.com/usedbytes/ducky-tools/lib/iap"
 	"github.com/usedbytes/ducky-tools/lib/update"
 	"github.com/usedbytes/log"
 )
@@ -83,10 +86,82 @@ func extractAction(ctx *cli.Context) error {
 	return err
 }
 
+func iapTestAction(ctx *cli.Context) error {
+	log.Println(">>> Loading update file...")
+	u, _, err := loadUpdateFile(ctx)
+	if err != nil {
+		return err
+	}
+
+	vid, pid := u.GetAPVIDPID()
+
+	log.Println(">>> Connecting in AP mode...")
+	iapCtx, err := iap.NewContext(uint16(vid), uint16(pid))
+	if err != nil {
+		return err
+	}
+	defer iapCtx.Close()
+
+	iapCtx.SetExtraCRCData(u.GetCRCData(update.Internal))
+
+	log.Println(">>> Attempt ReadData...")
+	data := make([]byte, 64)
+	addr := uint32(0x2800)
+	_, err = iapCtx.ReadData(addr, data[:])
+	if err != nil {
+		return err
+	}
+	log.Printf("AP 0x%04x (%d)\n", addr, addr)
+	log.Println(hex.Dump(data))
+
+	log.Print(">>> Attempt Ping... ")
+	pong, _ := iapCtx.Ping(42)
+	log.Println("pong")
+
+	log.Println(">>> Reset to IAP mode...")
+	iapCtx.Reset(false)
+
+	time.Sleep(1 * time.Second)
+
+	vid, pid = u.GetIAPVIDPID()
+
+	log.Println(">>> Connecting in IAP mode...")
+	iapCtx, err = iap.NewContext(vid, pid)
+	if err != nil {
+		return err
+	}
+	defer iapCtx.Close()
+
+	iapCtx.SetExtraCRCData(u.GetCRCData(update.Internal))
+
+	log.Println(">>> Attempt ReadData...")
+	addr = uint32(0x3c00)
+	_, err = iapCtx.ReadData(addr, data[:])
+	if err != nil {
+		return err
+	}
+	log.Printf("IAP 0x%04x (%d)\n", addr, addr)
+	log.Println(hex.Dump(data))
+
+	log.Print(">>> Attempt Ping (should time out)... ")
+	pong, err = iapCtx.Ping(42)
+	if pong && err != nil {
+		return errors.New("Ping() shouldn't work in IAP mode")
+	}
+	log.Println("timed out")
+
+	log.Println("Reset back to AP mode...")
+	iapCtx.Reset(false)
+
+	log.Println("Success!")
+
+	return nil
+}
+
 func main() {
 	app := &cli.App{
-		Name:      "ducky",
-		Usage:     "A tool for working with Ducky One firmware udpates",
+		Name:  "ducky",
+		Usage: "A tool for working with Ducky One firmware udpates",
 		// Just ignore errors - we'll handle them ourselves in main()
 		ExitErrHandler: func(c *cli.Context, e error) {},
 		Flags: []cli.Flag{
@@ -102,9 +177,9 @@ func main() {
 
 	app.Commands = []*cli.Command{
 		{
-			Name: "extract",
+			Name:      "extract",
 			ArgsUsage: "INPUT_FILE",
-			Action: extractAction,
+			Action:    extractAction,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:     "version",
@@ -112,6 +187,25 @@ func main() {
 					Usage:    "Specify the updater version if it can't be found automatically",
 					Required: false,
 					Value:    "1.03r",
+				},
+			},
+		},
+		{
+			Name: "iap",
+			Subcommands: []*cli.Command{
+				{
+					Name:      "test",
+					ArgsUsage: "INPUT_FILE",
+					Action:    iapTestAction,
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:     "version",
+							Aliases:  []string{"V"},
+							Usage:    "Specify the updater version if it can't be found automatically",
+							Required: false,
+							Value:    "1.03r",
+						},
+					},
 				},
 			},
 		},
