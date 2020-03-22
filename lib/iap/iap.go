d390650f8ed34b08ca7d5034309e3fbaeec045e4
Brian Starkey <stark3y@gmail.com>
2020-03-22 15:58:37 +0000

    lib/iap: Implement initial IAP setup and ReadData

---
+++ lib/iap/iap.go
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2020 Brian Starkey <stark3y@gmail.com>
+package iap
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"time"
+
+	"github.com/google/gousb"
+	"github.com/pkg/errors"
+	"github.com/sigurn/crc16"
+	"github.com/usedbytes/log"
+)
+
+type Context struct {
+	bg    context.Context
+	ctx   *gousb.Context
+	dev   *gousb.Device
+	cfg   *gousb.Config
+	intf  *gousb.Interface
+	outEp *gousb.OutEndpoint
+	inEp  *gousb.InEndpoint
+
+	crct     *crc16.Table
+	crcExtra []byte
+}
+
+func NewContext(vid, pid uint16) (*Context, error) {
+	ctx := &Context{
+		bg:   context.Background(),
+		ctx:  gousb.NewContext(),
+		crct: crc16.MakeTable(crc16.CRC16_XMODEM),
+	}
+
+	var err error
+	ctx.dev, err = ctx.ctx.OpenDeviceWithVIDPID(gousb.ID(vid), gousb.ID(pid))
+	if err != nil {
+		// Now I understand what go2 is trying to improve
+		ctx.Close()
+		return nil, err
+	} else if ctx.dev == nil {
+		ctx.Close()
+		return nil, errors.New("Couldn't find a matching device")
+	}
+
+	err = ctx.dev.SetAutoDetach(true)
+	if err != nil {
+		ctx.Close()
+		return nil, err
+	}
+
+	num, err := ctx.dev.ActiveConfigNum()
+	if err != nil {
+		ctx.Close()
+		return nil, err
+	}
+
+	ctx.cfg, err = ctx.dev.Config(num)
+	if err != nil {
+		ctx.Close()
+		return nil, err
+	}
+
+	ctx.intf, err = ctx.cfg.Interface(1, 0)
+	if err != nil {
+		ctx.Close()
+		return nil, err
+	}
+
+	for _, v := range ctx.intf.Setting.Endpoints {
+		if v.TransferType != gousb.TransferTypeInterrupt {
+			ctx.Close()
+			return nil, errors.New("Expected to find interrupt endpoints")
+		}
+
+		if v.Direction == gousb.EndpointDirectionIn {
+			if ctx.inEp != nil {
+				ctx.Close()
+				return nil, errors.New("Found multiple In endpoints")
+			}
+			ctx.inEp, err = ctx.intf.InEndpoint(v.Number)
+			if err != nil {
+				ctx.Close()
+				return nil, err
+			}
+		}
+
+		if v.Direction == gousb.EndpointDirectionOut {
+			if ctx.outEp != nil {
+				ctx.Close()
+				return nil, errors.New("Found multiple Out endpoints")
+			}
+			ctx.outEp, err = ctx.intf.OutEndpoint(v.Number)
+			if err != nil {
+				ctx.Close()
+				return nil, err
+			}
+		}
+	}
+
+	log.Verboseln(ctx.intf)
+	log.Verboseln(ctx.inEp)
+	log.Verboseln(ctx.outEp)
+
+	return ctx, nil
+}
+
+func (c *Context) Close() {
+	if c.intf != nil {
+		c.intf.Close()
+		c.intf = nil
+	}
+	if c.cfg != nil {
+		c.cfg.Close()
+		c.cfg = nil
+	}
+	if c.dev != nil {
+		c.dev.Close()
+		c.dev = nil
+	}
+	if c.ctx != nil {
+		c.ctx.Close()
+		c.ctx = nil
+	}
+}
+
+func (c *Context) sendPacket(packet []byte) error {
+	if len(packet) < 0x40 {
+		packet = append(packet, make([]byte, 0x40-len(packet))...)
+	}
+
+	crc := crc16.Checksum(packet, c.crct)
+	crc = crc16.Update(crc, c.crcExtra, c.crct)
+
+	binary.LittleEndian.PutUint16(packet[2:], crc)
+
+	log.Verbose("Write\n", hex.Dump(packet))
+
+	n, err := c.outEp.Write(packet)
+	if err != nil {
+		return err
+	} else if n != len(packet) {
+		return errors.New("Short write")
+	}
+
+	return nil
+}
+
+func (c *Context) readPacket(packet []byte) (int, error) {
+	if len(packet) != 0x40 {
+		return 0, errors.New("Read transfers must be 64 bytes")
+	}
+
+	to, cancel := context.WithTimeout(c.bg, 1 * time.Second)
+	defer cancel()
+
+	n, err := c.inEp.ReadContext(to, packet)
+	if err == nil {
+		log.Verbose("Read", n, "\n", hex.Dump(packet))
+	}
+
+	return n, err
+}
+
+func (c *Context) ReadData(start uint32, data []byte) (int, error) {
+	packet := []byte{
+		0x01, 0x02,
+		0x00, 0x00, // CRC
+		0x00, 0x00, 0x00, 0x00, // Start
+		0x00, 0x00, 0x00, 0x00, // End
+	}
+
+	if len(data) > 0x40 {
+		return 0, errors.New("Too long. Split not implemented")
+	}
+
+	binary.LittleEndian.PutUint32(packet[4:], start)
+	binary.LittleEndian.PutUint32(packet[8:], start+uint32(len(data)-1))
+
+	err := c.sendPacket(packet)
+	if err != nil {
+		return 0, err
+	}
+
+	rxbuf := data
+	if len(rxbuf) < 0x40 {
+		rxbuf = make([]byte, 64)
+	}
+
+	n, err := c.readPacket(rxbuf)
+	if len(rxbuf) > len(data) {
+		copy(data, rxbuf)
+	}
+
+	return n, err
+}
