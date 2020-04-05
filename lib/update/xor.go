995fce02efbe4c6a20e9c7df2031693a1ab72824
Brian Starkey <stark3y@gmail.com>
2020-04-05 12:58:40 +0100

    lib/update/xor: Fix out-of-bounds access
    If len(data) % len(key) != 0

--- lib/update/xor.go
+++ lib/update/xor.go
@@ -5,7 +5,7 @@ package update
 func XORDecode(data []byte, key []byte, withIdx bool) []byte {
 	res := make([]byte, len(data))
 	for i := 0; i < len(data); {
-		for j := 0; j < len(key); i, j = i+1, j+1 {
+		for j := 0; (j < len(key)) && (i < len(data)); i, j = i+1, j+1 {
 			v := data[i] ^ key[j]
 			if withIdx {
 				v = v ^ byte(i)
