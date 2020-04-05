a243ca981015ea93a9f0d7668bfff9d5e9edfd42
Brian Starkey <stark3y@gmail.com>
2020-04-05 15:46:12 +0100

    lib/update/toml: Support for saving out to .toml

--- lib/update/toml.go
+++ lib/update/toml.go
@@ -4,30 +4,33 @@ package update
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/pkg/errors"
 )
 
 type tomlBlob struct {
-	CheckCRC     uint16 `toml:"check_crc"`
+	CheckCRC     uint16 `toml:"check_crc,omitempty"`
 	FileEncoded  bool   `toml:"file_encoded"`
 	XferEncoded  bool   `toml:"xfer_encoded"`
-	XferKeyFile  string `toml:"xfer_key_file"`
-	DataFile     string `toml:"data_file"`
-	ExtraCRCFile string `toml:"extra_crc_data_file"`
+	XferKeyFile  string `toml:"xfer_key_file,omitempty"`
+	DataFile     string `toml:"data_file,omitempty"`
+	ExtraCRCFile string `toml:"extra_crc_data_file,omitempty"`
 }
 
 type tomlUpdate struct {
-	Name         string              `toml:"name"`
-	IAPVerStr    string              `toml:"iap_version"`
-	VerStr       string              `toml:"version"`
-	FileKey      uint32              `toml:"file_key"`
-	ApVidPid     []uint16            `toml:"ap_vid_pid"`
-	IapVidPid    []uint16            `toml:"iap_vid_pid"`
-	Images       map[string]tomlBlob `toml:"images"`
+	Name      string               `toml:"name"`
+	IAPVerStr string               `toml:"iap_version"`
+	VerStr    string               `toml:"version"`
+	FileKey   uint32               `toml:"file_key,omitzero"`
+	ApVidPid  []uint16             `toml:"ap_vid_pid"`
+	IapVidPid []uint16             `toml:"iap_vid_pid"`
+	Images    map[string]*tomlBlob `toml:"images"`
 }
 
 func LoadTOMLUpdate(file string) (*Update, error) {
@@ -146,4 +149,94 @@ func LoadTOMLUpdate(file string) (*Update, error) {
 	}
 
 	return &u, nil
+}
+
+func removeIfTrue(file string, cond *bool) {
+	if *cond {
+		os.Remove(file)
+	}
+}
+
+func (u *Update) WriteTOML(file string) error {
+	tu := tomlUpdate{
+		Name:      u.Name,
+		IAPVerStr: u.IAPVersion.String(),
+		VerStr:    u.Version.String(),
+		FileKey:   u.FileKey,
+		ApVidPid:  []uint16{u.APVID, u.APPID},
+		IapVidPid: []uint16{u.IAPVID, u.IAPPID},
+		Images:    make(map[string]*tomlBlob),
+	}
+
+	fail := true
+
+	dir := filepath.Dir(file)
+	base := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
+
+	for k, v := range u.Images {
+		tb := &tomlBlob{
+			CheckCRC:    v.CheckCRC,
+			FileEncoded: false,
+		}
+		tu.Images[k.String()] = tb
+
+		if len(v.Data) != 0 {
+			var data []byte
+			if len(v.XferKey) != 0 {
+				tb.XferEncoded = false
+				data = XORDecode(v.Data, v.XferKey, false)
+			} else {
+				tb.XferEncoded = true
+				data = v.Data
+			}
+
+			tb.DataFile = fmt.Sprintf("%s.%s.bin", base, k)
+			fullname := filepath.Join(dir, tb.DataFile)
+
+			err := ioutil.WriteFile(fullname, data, 0644)
+			if err != nil {
+				return err
+			}
+			defer removeIfTrue(fullname, &fail)
+		}
+
+		if len(v.ExtraCRC) != 0 {
+			tb.ExtraCRCFile = fmt.Sprintf("%s.%s.extracrc.bin", base, k)
+			fullname := filepath.Join(dir, tb.ExtraCRCFile)
+
+			err := ioutil.WriteFile(fullname, v.ExtraCRC, 0644)
+			if err != nil {
+				return err
+			}
+			defer removeIfTrue(fullname, &fail)
+		}
+
+		if len(v.XferKey) != 0 {
+			tb.XferKeyFile = fmt.Sprintf("%s.%s.xferkey.bin", base, k)
+			fullname := filepath.Join(dir, tb.XferKeyFile)
+
+			err := ioutil.WriteFile(fullname, v.XferKey, 0644)
+			if err != nil {
+				return err
+			}
+			defer removeIfTrue(fullname, &fail)
+		}
+	}
+
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer removeIfTrue(file, &fail)
+
+	enc := toml.NewEncoder(f)
+	err = enc.Encode(&tu)
+	if err != nil {
+		return err
+	}
+
+	// Prevent cleanup
+	fail = false
+
+	return nil
 }
