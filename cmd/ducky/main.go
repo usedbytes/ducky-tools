a7530fd044f885b9d34a0fc433dad2cc67dfecea
Brian Starkey <stark3y@gmail.com>
2020-11-20 23:01:31 +0000

    cmd/ducky: Implement dump for iap2
    I haven't figured out where the "size" field is in the info queries, so
    this just dumps everything until a transaction fails. It takes about 5
    minutes on my machine.
    
    libusb throws a lot of "interrupted" errors, but I think the same
    happened for the One. It seems harmless.

--- cmd/ducky/main.go
+++ cmd/ducky/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/binary"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,6 +19,7 @@ import (
 	"github.com/usedbytes/log"
 
 	"github.com/sigurn/crc16"
+	"github.com/cheggaaa/pb/v3"
 )
 
 func loadUpdateFile(ctx *cli.Context) (*config.Config, error) {
@@ -535,6 +537,241 @@ func iap2TestAction(ctx *cli.Context) error {
 	return nil
 }
 
+func iap2DumpAction(ctx *cli.Context) error {
+	cfg, err := loadUpdateFile(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(cfg.Devices) != 1 || len(cfg.Devices[0].Firmwares) != 1 {
+		// XXX: Need to implement device (and firmware) selection
+		return errors.New("dump requires a single device with a single firmware")
+	}
+
+	fw := cfg.Devices[0].Firmwares[0]
+	img, ok := fw.Images["internal"]
+	if !ok || len(img.XferKey) == 0 {
+		return errors.New("dump requires an XferKey for the internal image")
+	}
+	key := img.XferKey
+
+	iapCtx, err := iap.NewContext(cfg.Devices[0])
+	if err != nil {
+		return err
+	}
+
+	err = iapCtx.Reset(false)
+	if err != nil {
+		return err
+	}
+
+	err = iapCtx.Reset(true)
+	if err != nil {
+		return err
+	}
+
+	proto, ok := iapCtx.Protocol().(*iap.ProtocolOne2)
+	if !ok {
+		return errors.New("wrong protocol version")
+	}
+
+	defer proto.Reset(false)
+
+	// The checksum in the firmware applies the XOR key to the data being
+	// checksummed, and adds together all the results. Something like:
+	//
+	// checksum(addr, length) =
+	//    mem[addr] ^ key[0] + mem[addr+1] ^ key[1] ... + mem[addr+length-1] ^ key[length-1]
+	//
+	// For compactness, lets re-write that as:
+	//
+	//     a ^ k0 + b ^ k1 + c ^ k2 ...
+	//
+	// We know all the 'k' values, we want to find a, b, c...
+	//
+	// This is easy assuming we can keep adding extra values to the and of
+	// the checksum. e.g.
+	// checksum(a..c) is:
+	//    a ^ k0 + b ^ k1 + c ^ k2
+	// and checksum(a..d) is:
+	//    a ^ k0 + b ^ k1 + c ^ k2 + d ^ k3
+	// therefore checksum(a..d) - checksum(a..c) is:
+	//    (a ^ k0 + b ^ k1 + c ^ k2 + d ^ k3) - (a ^ k0 + b ^ k1 + c ^ k2)
+	// which simplifies to just (d ^ k3), and we know k3, so we can find d.
+	//    d = (checksum(a..d) - checksum(a..c)) ^ k3
+	//
+	// This is fine so long as we can keep _extending_ the checksum.
+	// However, the firmware imposes some restrictions:
+	//   1. addr >= 0x400
+	//   2. length >= 0x1000
+	//
+	// This means the first value we can find by extending the checksum is
+	// at address 0x1400, and we can't find any values lower than that via
+	// this method. (checksum(a..c) in the analogy above effectively becomes
+	// checksum(0x400..0x1400))
+	//
+	// Unfortunately, extending the checksum "down" is not as
+	// straightforward as extending it up.
+	//
+	// If we had checksum(b..d) and checksum(a..d), we can't simply subtract
+	// one from the other to find 'a', because the key always starts from
+	// k0 for the lowest address. i.e.:
+	// checksum(a..d) is:
+	//    a ^ k0 + b ^ k1 + c ^ k2 + d ^ k3
+	// and checksum(b..d) is:
+	//    b ^ k0 + c ^ k1 + d ^ k2
+	// We can't directly simplify this, because in one we have (b ^ k0) and
+	// in the other (b ^ k1), so one doesn't simply subtract from the other.
+	//
+	// Thankfully, we can combine the two approaches.
+	//
+	// If we take checksum(a..d):
+	//    a ^ k0 + b ^ k1 + c ^ k2 + d ^ k3
+	// and we have already found b, c and d with the approach above, then
+	// we can calculate the part from 'b' onwards for ourselves:
+	//    b ^ k1 + c ^ k2 + d ^ k2
+	// Subtracting that from checksum(a..d) leaves just (a ^ k0), where
+	// we know k0, and we can find 'a'.
+
+
+	// To start with, we find the data from 0x1400-0x2400 using the
+	// extension method. Note that we can apply the XOR decode to all the
+	// data at the end, we don't need to decode the values as we read them.
+	// However we do pad the start of the buffer with 0x1000 bytes, and
+	// start the decode from there, to make sure the right key values are
+	// used for the right entries in the buffer.
+	// Technically, we don't need the full 0x1000 bytes at the start - only
+	// enough to align to the key length (0x3c).
+
+	data := make([]byte, 0x2000)
+	baseline := uint32(0)
+
+	log.Println("Read upwards, 0x1400-0x2400...")
+	bar := pb.StartNew(0x1000)
+	bar.SetMaxWidth(80)
+
+	for i := uint32(0); i <= 0x1000; i += 4 {
+		csum, err := proto.Checksum(0x400, 0x1000 + i)
+		if err != nil {
+			return err
+		}
+
+		if i > 0 {
+			val := csum - baseline
+			binary.LittleEndian.PutUint32(data[0x1000 + (i - 4):], val)
+		}
+		baseline = csum
+
+		bar.SetCurrent(int64(i))
+	}
+	bar.Finish()
+
+	decoded := xor.Decode(data, key, false)
+
+	// Now data contains 0x1000 bytes of the XOR key repeating (garbage),
+	// followed by 0x1000 bytes of valid decoded data, representing
+	// addresses 0x1400-0x2400
+	copy(data, decoded)
+
+	// Part 2 is to work downwards from 0x1400 to 0x400, using the second
+	// method
+	log.Println("Read downwards, 0x1400-0x400...")
+	bar = pb.StartNew(0x1000)
+	bar.SetMaxWidth(80)
+
+	tmp := []byte{0, 0, 0, 0}
+	for start := uint32(0x1400-4); start >= 0x400; start -= 4 {
+		// We always checksum 0x1000 bytes, starting at successively
+		// lower addresses
+		csum, err := proto.Checksum(start, 0x1000)
+		if err != nil {
+			return err
+		}
+
+		// Address 0x400 is offset 0 in our data buffer
+		idx := start - 0x400
+
+		// Encode 0x1000 bytes of the data we already found, the same
+		// as the bootloader will do.  The first 4 bytes for us are
+		// meaningless, but that doesn't matter.
+		enc := xor.Decode(data[idx:idx+0x1000], key, false)
+
+		// Then checksum from the 4th byte onwards (i.e. b..d from
+		// the analogy), to give us the baseline
+		baseline := iap.Checksum(enc[4:])
+
+		// Subtract one from the other, to leave a ^ k0
+		val := csum - baseline
+
+		// And this time, we need to XOR with the first 4 bytes of
+		// the key (k0) for each value individually.
+		binary.LittleEndian.PutUint32(tmp, val)
+		copy(data[idx:], xor.Decode(tmp, key, false))
+
+		bar.Add(4)
+	}
+	bar.Finish()
+
+	// If all went well, we've now got 0x2000 bytes of valid data, for
+	// addresses 0x400-0x2400!
+
+	var f *os.File
+	fname := ctx.String("outfile")
+	if len(fname) != 0 {
+		f, err = os.Create(fname)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		n, err := f.Write(data)
+		if err != nil {
+			return err
+		} else if n != len(data) {
+			return errors.New("short write to outfile")
+		}
+	}
+
+	// Now read upwards until checksum fails (presumably the top of flash)
+	// We'll work in smaller chunks to hopefully manage to get all data
+	pageSize := uint32(256)
+	data = data[:0x1000+pageSize]
+	for page := 0; page < 256; page++ {
+		start := uint32(page * 256 + 0x2400)
+		log.Printf("Read upwards, 0x%04x-0x%04x...\n", start, start + pageSize)
+
+		bar := pb.StartNew(int(pageSize))
+		bar.SetMaxWidth(80)
+		for i := uint32(0); i <= pageSize; i += 4 {
+			csum, err := proto.Checksum(start-0x1000, 0x1000 + i)
+			if err != nil {
+				return err
+			}
+
+			if i > 0 {
+				val := csum - baseline
+				binary.LittleEndian.PutUint32(data[0x1000 + (i - 4):], val)
+			}
+			baseline = csum
+
+			bar.SetCurrent(int64(i))
+		}
+		bar.Finish()
+
+		decoded := xor.Decode(data, key, false)
+		if f != nil {
+			n, err := f.Write(decoded[0x1000:])
+			if err != nil {
+				return err
+			} else if n != len(decoded[0x1000:]) {
+				return errors.New("short write to outfile")
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	app := &cli.App{
 		Name:  "ducky",
@@ -628,6 +865,19 @@ func main() {
 					Name:      "test",
 					Action:    iap2TestAction,
 					ArgsUsage: "INPUT_FILE",
+				},
+				{
+					Name:      "dump",
+					Action:    iap2DumpAction,
+					ArgsUsage: "INPUT_FILE",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:     "outfile",
+							Aliases:  []string{"o"},
+							Usage:    "Output file to write data to",
+							Required: false,
+						},
+					},
 				},
 			},
 		},
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/cheggaaa/pb/v3 v3.0.5
 	github.com/google/gousb v0.0.0-20190812193832-18f4c1d8a750
 	github.com/pkg/errors v0.9.1
 	github.com/sigurn/crc16 v0.0.0-20160107003519-da416fad5162
